e526c5d4a589cba40e5c18fbbe86a196db3f9d8f
bolat <bolatmamyr123@gmail.com>
2023-11-23 21:30:26 +0600

    Finished UI

---
+++ app/src/main/java/com/example/newsapp/ui/news/NewsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.newsapp.ui.news
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentNewsBinding
+
+class NewsFragment : Fragment() {
+
+    private var _binding: FragmentNewsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNewsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivFavorites.setOnClickListener {
+            findNavController().navigate(R.id.action_newsFragment_to_savedFragment)
+        }
+    }
+
+}
--- app/src/main/java/com/example/newsapp/ui/saved/SavedFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/saved/SavedFragment.kt
@@ -1,4 +1,4 @@
-package com.example.newsapp.ui
+package com.example.newsapp.ui.saved
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,20 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.R
-import com.example.newsapp.databinding.FragmentNewsBinding
 
-class NewsFragment : Fragment() {
-
-    private var _binding: FragmentNewsBinding? = null
-    private val binding get() = _binding
-
+class SavedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentNewsBinding.inflate(layoutInflater)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved, container, false)
     }
-
 }
